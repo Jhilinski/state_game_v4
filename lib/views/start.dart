ff037dec3a8a8b92dece7587f527995e9ce45144
Joe Hilinski <jhilinski@yahoo.com>
2022-10-11 18:28:56 -0400

    add audio checkbox

--- lib/views/start.dart
+++ lib/views/start.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -13,6 +15,7 @@ class _StartPageState extends State<StartPage> {
 
   String playerName = 'Player';
   String savedName = 'Player';
+  bool? checkBoxValue = true;
 
   //reference our box
   final _myBox = Hive.box('myBox');
@@ -20,64 +23,74 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Name The State Game'),
-        ),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  'Hello! $playerName',
-                  style: const TextStyle(fontSize: 25.0),
-                  textAlign: TextAlign.center,
-                ),
-                Image.asset(
-                  'assets/images/states_no_name/USA Color Mao No Names300.png',
-                ),
-                const Text(
-                  'Do You Know Your State Shape?',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-
-                //Text('Saved Name $savedName'),
-                TextField(
-                  textAlign: TextAlign.center,
-                  controller: _playerTextController,
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.add),
-                    border: OutlineInputBorder(),
-                    hintText: 'Type Your Name!',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: const Text('Name The State Game'),
+          ),
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Hello! $playerName',
+                    style: const TextStyle(fontSize: 25.0),
+                    textAlign: TextAlign.center,
                   ),
-				  //onChanged is called whenever we add or delete something on Text Field
-                  onSubmitted: (String str){
-                    setState((){
-                      playerName = str;
-                      _myBox.put(1, playerName);
-                      savedName = (_myBox.get(1));
-                    });
-                  }
-                ),
-                //displaying input text
-                //new Text(playerName),
-              
-                
-                  
-                const SizedBox(height: 5.0),
-              
-                ElevatedButton(
-                  child: const Text('Start'),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/page_one');
-                },
-                )
-                
+                  Image.asset(
+                    'assets/images/states_no_name/USA Color Mao No Names300.png',
+                  ),
+                  const Text(
+                    'Do You Know Your State Shape?',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Checkbox(
+                        value: checkBoxValue,
+                        onChanged: (bool? value) {
+                          print(value);
+                          setState(() {
+                            checkBoxValue = value;
+                          });
+                        },
+                      ),
+                      const Text('Audio?'),
+                    ],
+                  ),
+
+                  //Text('Saved Name $savedName'),
+                  TextField(
+                      textAlign: TextAlign.center,
+                      controller: _playerTextController,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.add),
+                        border: OutlineInputBorder(),
+                        hintText: 'Type Your Name!',
+                      ),
+                      //onChanged is called whenever we add or delete something on Text Field
+                      onSubmitted: (String str) {
+                        setState(() {
+                          playerName = str;
+                          _myBox.put(1, playerName);
+                          savedName = (_myBox.get(1));
+                        });
+                      }),
+                  //displaying input text
+                  //new Text(playerName),
+
+                  const SizedBox(height: 5.0),
+
+                  ElevatedButton(
+                    child: const Text('Start'),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/page_one');
+                    },
+                  )
                 ]),
-              ),
-        )
-     );
+          ),
+        ));
   }
 }
